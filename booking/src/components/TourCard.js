a8f1dc57cb562fbf748d59355bd090003c927d98
nyima6999 <nyima6999@gmail.com>
2022-06-11 23:17:49 -0700

    api called

--- booking/src/components/TourCard.js
+++ booking/src/components/TourCard.js
@@ -4,61 +4,84 @@ import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import { AccessTime } from "@mui/icons-material";
 import Rating from "@mui/material/Rating";
-import { createTheme } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material";
 
-const TourCard = () => {
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: {
+            variant: "body2",
+          },
+          style: {
+            fontSize: 11,
+          },
+        },
+        {
+          props: {
+            variants: "body3",
+          },
+          style: {
+            fontSize: 9,
+          },
+        },
+      ],
+    },
+  },
+});
+
+const TourCard = ({ tour }) => {
   return (
     <Grid item xs={3}>
-      <Paper elevation={3}>
-        <img
-          src="https://images.pexels.com/photos/306381/pexels-photo-306381.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="nigara falls"
-          className="img"
-        />
-        <Box paddingX={1}>
-          <Typography variant="subtitle1" component="h2">
-            {" "}
-            Immerse into the falls
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            alignItem: "center",
-          }}
-        >
-          <AccessTime sx={{ width: 14 }} />
-          <Typography variant="body2" component="p" marginLeft={0.5}>
-            5 hours
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            alignItem: "center",
-          }}
-          marginTop={3}
-        >
-          <Rating
-            name="read-only"
-            value={4.5}
-            readOnly
-            precision={0.5}
-            size="small"
-          />
-          <Typography variant="body2" component="p" marginLeft={0.5}>
-            4.5
-          </Typography>
-          <Typography variant="body3" component="p" marginLeft={1.5}>
-            (655 reviews)
-          </Typography>
-        </Box>
-        <Box>
-          <Typography variant="h6" component="h3" marginTop={0}>
-            From USD $480
-          </Typography>
-        </Box>
-      </Paper>
+      <ThemeProvider theme={theme}>
+        <Paper elevation={3}>
+          <img src={tour.image} alt="nigara falls" className="img" />
+          <Box paddingX={1}>
+            <Typography variant="subtitle1" component="h2">
+              {" "}
+              {tour.name}
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItem: "center",
+            }}
+          >
+            <AccessTime sx={{ width: 14 }} />
+            <Typography variant="body2" component="p" marginLeft={0.5}>
+              {tour.duration} hours
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItem: "center",
+            }}
+            marginTop={3}
+          >
+            <Rating
+              name="read-only"
+              value={tour.rating}
+              readOnly
+              precision={0.5}
+              size="small"
+            />
+            <Typography variant="body2" component="p" marginLeft={0.5}>
+              {tour.rating}
+            </Typography>
+            <Typography variant="body3" component="p" marginLeft={1.5}>
+              ({tour.numberOfReviews}reviews)
+            </Typography>
+          </Box>
+          <Box>
+            <Typography variant="h6" component="h3" marginTop={0}>
+              From USD ${tour.price}
+            </Typography>
+          </Box>
+        </Paper>
+      </ThemeProvider>
     </Grid>
   );
 };
