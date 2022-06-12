026f91bde8eef3374d58b6094f12def14feea3c1
nyima6999 <nyima6999@gmail.com>
2022-06-11 20:42:03 -0700

    some style added to the paper component

--- booking/src/components/TourCard.js
+++ booking/src/components/TourCard.js
@@ -3,6 +3,8 @@ import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import { AccessTime } from "@mui/icons-material";
+import Rating from "@mui/material/Rating";
+import { createTheme } from "@mui/material";
 
 const TourCard = () => {
   return (
@@ -26,8 +28,34 @@ const TourCard = () => {
           }}
         >
           <AccessTime sx={{ width: 14 }} />
-          <Typography variant="body2" component="p">
+          <Typography variant="body2" component="p" marginLeft={0.5}>
             5 hours
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            alignItem: "center",
+          }}
+          marginTop={3}
+        >
+          <Rating
+            name="read-only"
+            value={4.5}
+            readOnly
+            precision={0.5}
+            size="small"
+          />
+          <Typography variant="body2" component="p" marginLeft={0.5}>
+            4.5
+          </Typography>
+          <Typography variant="body3" component="p" marginLeft={1.5}>
+            (655 reviews)
+          </Typography>
+        </Box>
+        <Box>
+          <Typography variant="h6" component="h3" marginTop={0}>
+            From USD $480
           </Typography>
         </Box>
       </Paper>
